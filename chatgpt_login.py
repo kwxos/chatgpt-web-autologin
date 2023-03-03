643188aa53d7700460439de58b5d85b499837a84
daokun.lv <daokun.lv@dhc.com.cn>
2023-03-04 02:01:21 +0800

    修正重启失败问题

--- chatgpt_login.py
+++ chatgpt_login.py
@@ -14,8 +14,8 @@ CONFIG_PATH = 'config.yaml'
 log = logging.basicConfig(level=logging.INFO,
                           format="%(asctime)s-%(name)s-%(levelname)s-%(message)s")
 # 定义随机时间范围
-min_time = 0.1 * 60  # 最小等待时间（分钟）
-max_time = 0.5 * 60  # 最大等待时间（分钟）
+min_time = 5 * 60  # 最小等待时间（分钟）
+max_time = 7 * 60  # 最大等待时间（分钟）
 
 
 # 配置文件
@@ -65,12 +65,13 @@ if __name__ == '__main__':
             token = openAIAuth.get_access_token()
             logging.info('token:%s' % token)
             os.environ['OPENAI_ACCESS_TOKEN'] = token
-            process = subprocess.Popen(['pnpm', 'run', 'start'], cwd="/app", stdout=subprocess.PIPE)
+            process = subprocess.Popen(['pnpm', 'run', 'start'], cwd="/app", stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
             # 随机等待一段时间
             wait_time = config.wait_time if config.wait_time else random.randint(min_time, max_time)
             logging.info(f"等待 {wait_time} 分钟后更新token")
             start_time = time.time()
-            end_time = start_time + wait_time
+            end_time = start_time + (wait_time * 60)
             while True:
                 # 等待输出流可读或超时
                 ready = select.select([process.stdout], [], [], 1)
@@ -79,20 +80,23 @@ if __name__ == '__main__':
                     output = process.stdout.readline().decode('utf-8')
                     # 如果程序不再运行则跳出，重新获取token
                     if output == '' and process.poll() is not None:
-                        logging.info("已关闭系统")
-                        process.kill()
-                        time.sleep(30)
+                        logging.debug("已关闭系统")
+                        process.terminate()
+                        process.wait(30)
+                        logging.debug("已关闭系统 code:%s" % process.poll())
                         break
                     if output:
                         logging.info(output.strip())
                 # 超过定时时间后，关闭当前nodejs并执行下一轮
                 if time.time() > end_time:
-                    logging.info("关闭系统")
-                    process.kill()
-                    time.sleep(30)
+                    logging.debug("关闭系统")
+                    process.terminate()
+                    process.wait(30)
+                    logging.debug("关闭系统 code:%s" % process.poll())
                     break
         except Exception as e:
             logging.exception(e)
             error_count = error_count + 1
+            logging.debug("发生异常：%s" % error_count)
             if error_count > 5:
                 break
